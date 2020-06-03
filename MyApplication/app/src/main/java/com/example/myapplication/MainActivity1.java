a24f35b0608fc1abe91cdb58a7f718e85d0e7bb7
ywh <ywh@qq.com>
2020-06-04 00:53:17 +0800

    第一次提交

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity1.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity1.java
@@ -1,17 +1,17 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
         TextView  textView = findViewById(R.id.text) ;
-        textView.setText("gitdemo");
+        textView.setText("git");
     }
 }
